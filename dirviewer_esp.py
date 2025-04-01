fb29ab11569d2155a6181c345a8474b002b6226f
Mauricio Rodríguez <161236366+ImMau14@users.noreply.github.com>
2025-04-01 10:03:52 -0400

    Update dirviewer_esp.py
    Se añadió una pequeña función para saber si se está ejecutando en Google Colab o de forma local.

--- dirviewer_esp.py
+++ dirviewer_esp.py
@@ -4,12 +4,19 @@ from os import listdir, getcwd, system, name
 COLUMNS = 3
 COLUMNS_WIDTH = 30
 
+try:
+	import google.colab
+	IS_COLAB = True
+
+except:
+	IS_COLAB = False
+
 # Macro (en realidad es una simulación de macro)
 CLEAR = lambda : system("cls" if name == 'nt' else "clear")
 
 # Función que espera a que el usuario presione enter
 def pressEnterToContinue():
-	input("\nPresione enter para continuar...")
+	input("\nPresione enter para continuar...\n")
 	CLEAR()
 
 # Función que muestra la lista en formato
@@ -22,6 +29,8 @@ def formatCenter(filesList):
 # Función que muestra los archivos.
 def showDir():
 	CLEAR()
+	if IS_COLAB:
+		print("\nEjecutando desde Colab")
 	print(f"Directorio actual: {getcwd()}", end = "\n\n")
 
 	try:
@@ -47,18 +56,19 @@ def menu():
 	match opcion:
 		case 1:
 			showDir()
-			return 1
+			return 1 # Continua el bucle principal
 
 		case 0:
-			return 0
+			return 0 # Se detiene el bucle principal
 
 		case _:
 			print("\nError: Opción no válida")
 			pressEnterToContinue()
-			return 1
+			return 1 # Continua el bucle principal
 
 if __name__ == "__main__":
 	# Bucle del menu
 	while True:
 		if not menu():
+			break
 			break
